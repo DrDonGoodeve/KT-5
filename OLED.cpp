53bdebfcc214fe26f08e367eb95c36119ee1f0d0
DrDonGoodeve <don@radiocode.ca>
2022-04-02 17:41:01 -0700

    Integrated OLED library from https://github.com/Harbys/pico-ssd1306

--- OLED.cpp
+++ OLED.cpp
@@ -1,8 +1,10 @@
 /******************************************************************************
  * OLED.cpp
  * 
- * Display driving for OLED 128x64 display. Code heavily borrowed from
- * pico example code for OLED driver over I2C.
+ * Display driving for OLED 128x32 display. Code relies on pico-ssd1306 from
+ * https://github.com/Harbys/pico-ssd1306. Looks to be well designed and
+ * clean code. Many thanks. Note this code is distributed under BSD v3
+ * license - which is reproduced in the source code for that library.
  * 
  * Don Goodeve  don@radiocode.ca  ground broken: 1Apr2022
  ******************************************************************************
@@ -11,8 +13,17 @@
 
 // Includes
 //*****************************************************************************
-#include "OLED.h"
 #include <stdio.h>
+#include "OLED.h"
+#include "pico-ssd1306/ssd1306.h"
+#include "pico-ssd1306/textRenderer/TextRenderer.h"
+#include "hardware/i2c.h"
+
+// Includes
+//*****************************************************************************
+#define kI2CPin0    (12)
+#define kI2CPin1    (13)
+#define kI2CFreq    (1000000)
 
 
 // Local functions
@@ -24,6 +35,39 @@
 //*****************************************************************************
 // Constructor - connect to OLED device and prepare to render
 OLED::OLED(void) {
+    // Init i2c0 controller
+    i2c_init(i2c0, kI2CFreq);
+
+    // Set up pins 12 and 13
+    gpio_set_function(kI2CPin0, GPIO_FUNC_I2C);
+    gpio_set_function(kI2CPin1, GPIO_FUNC_I2C);
+    gpio_pull_up(kI2CPin0);
+    gpio_pull_up(kI2CPin1);
+
+    // Pause for SSD1306 to self-initialize (assuming called soon after boot)
+    sleep_ms(250);
+
+    // Create a new display object at address 0x3D and size of 128x64
+    using namespace pico_ssd1306;
+    SSD1306 display = SSD1306(i2c0, 0x3D, Size::W128xH32);
+
+    // Here we rotate the display by 180 degrees, so that it's not upside down from my perspective
+    // If your screen is upside down try setting it to 1 or 0
+    display.setOrientation(0);
+
+    // Draw text on display
+    // After passing a pointer to display, we need to tell the function what font and text to use
+    // Available fonts are listed in textRenderer's readme
+    // Last we tell this function where to anchor the text
+    // Anchor means top left of what we draw
+    // We use \x escape to use chars by their hex numeration according to the ASCII table
+    drawText(&display, font_5x8, "\x24 \xba \xb2", 0 ,0);
+    drawText(&display, font_8x8, "\x24 \xba \xb2", 0 ,10);
+    drawText(&display, font_12x16, "\x24 \xba \xb2", 0 ,20);
+    drawText(&display, font_16x32, "\x24 \xba \xb2", 0 ,36);
+
+    // Send buffer to the display
+    display.sendBuffer();
 }
 
 /// Destructor 
--- README.md
+++ README.md
@@ -8,3 +8,9 @@ This measuring system consists of a transducer on a through-hull fitting consist
 The pulse train varies in magnitude and frequency with water speed. The original head instrument used a passive rectifier / low-pass filter to create a current through a moving coil galvanometer which moved the needle. Neat, delicate and unfortunately - broken. Water had got into the head and looks like it had corroded connections on the MCG which were not possible to fix with my soldering skills. My exploration broke what I assume was the bad connection where continuity through the galvanometer was lost. 
 
 However the hole in the boat remains - the right size for this instrument - I like the analog look and do not want to drop $350 for a new setup and new transducer. Being the Engineer I am - and clearly not busy enough - this seems like a fun project to try out on a modern, dirt-cheap microcontroller platform. This project runs on a Raspberry Pi Nano. The project documentation includes all source, pictures and design info. I hope it is useful to someone out there. Also I am documenting the design process through a youtube video - check out Don Goodeve on YouTube.
+
+Libraries:
+---
+Requires the OLED library from https://github.com/Harbys/pico-ssd1306 to build. This is required by OLED.cpp
+and is referenced from the CMakeLists.txt. This library is not part of this repository.
+
